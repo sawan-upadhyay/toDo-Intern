da93c23b92c28bd282abb0631c2ab05e0b72cdf0
Sawan Upadhyay <sawan24041996@gmail.com>
2024-08-22 15:17:26 +0530

    first commit

---
+++ src/components/TodosList.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
+import { RiCheckboxCircleFill } from "react-icons/ri";
+import { CiEdit } from "react-icons/ci";
+import { MdDelete } from "react-icons/md";
+
+const TodosList = ({todos,setTodos,setEditTodo}) => {
+  const handleComplete= ()=> {
+    setTodos(
+      todos.map((item)=> {
+        if (item.id===todos.id)
+          return {...item, completed:!item.completed} ;
+        else
+         return item;
+      })
+    );
+  };
+const handleEdit=( {id })=> {
+  const findTodo=todos.find((todo)=>todo.id===id);
+  setEditTodo(findTodo);
+ };
+
+  const handleDelete=({id})=> {
+    setTodos(todos.filter((todo)=> todo.id!==id));
+  }; 
+
+  return (
+    <div>
+      {
+        todos.map((todo)=> (
+          <li className="list-item" key={todo.id}>
+            <input 
+              type="text"
+              value={todo.title}
+              className={`list ${todo.completed ? "complete" : ""}`}
+              onChange={(event)=> event.preventDefault()}
+            />
+              <div>
+                <button className="button-complete task-button" onClick={()=> handleComplete(todo)}>
+              
+                {todo.completed ? <RiCheckboxCircleFill /> : <MdOutlineCheckBoxOutlineBlank /> }
+                </button>
+                <button 
+                     className="button-edit task-button"
+                     onClick={()=> handleEdit(todo)}
+                     >
+                     <CiEdit />
+                </button>
+                <button 
+                  className="button-delete task-button"
+                  onClick={()=> handleDelete(todo)}
+                >
+                  <MdDelete />
+                </button>
+
+              </div>
+          </li>
+        ))
+      }
+    </div>
+  )
+}
+
+export default TodosList
